8d06af5f1b9192e0d54d33ca152c31f030ac23e2
Tiago Torres Schmidt <tiago.schmidt@inf.ufrgs.br>
2021-11-16 10:23:02 -0300

    Adicionado mecanismo de busca de posição. Correção do quicksort.

--- trabFinalLib.py
+++ trabFinalLib.py
@@ -1,4 +1,4 @@
-class trieNode: #Objeto Nó da Árvore Trie. Contém o chararactere(s), se é fim de palavra e seus filhos.
+class trieNodeNames: #Objeto Nó da Árvore Trie de nomes. Contém o chararactere(s), se é fim de palavra e seus filhos.
     def __init__(self,char):
         self.char = char
         self.isEnd = False
@@ -6,9 +6,9 @@ class trieNode: #Objeto Nó da Árvore Trie. Contém o chararactere(s), se é fi
         self.positions = []
         self.children = {}
         
-class trie(object):#Objeto da Árvore Trie, em si. 
+class trieNames(object):#Objeto da Árvore Trie para nomes, em si. 
     def __init__(self):
-        self.root = trieNode("")
+        self.root = trieNodeNames("")
         
     def insert(self,line,nameHashTable,mNames):
         node = self.root
@@ -21,7 +21,7 @@ class trie(object):#Objeto da Árvore Trie, em si.
             if  char in node.children:
                 node = node.children[char]
             else:
-                newNode = trieNode(char)
+                newNode = trieNodeNames(char)
                 node.children[char] = newNode
                 node = newNode
 
@@ -36,6 +36,63 @@ class trie(object):#Objeto da Árvore Trie, em si.
     def depthFirstSearch(self, node, prefixWord):
         if node.isEnd:
             self.output.append([(prefixWord + node.char),node.soFifaId,node.positions])
+            
+        for child in node.children.values():
+            self.depthFirstSearch(child,prefixWord + node.char)
+            
+    def search(self,word):
+        node = self.root
+        for char in word:
+            if char in node.children:
+                node = node.children[char]
+            else:
+                return[]
+                
+        self.output = []
+        self.depthFirstSearch(node,word[:-1])
+        
+        return self.output
+
+class trieNodePositions: #Objeto Nó da Árvore Trie. Contém o chararactere(s), se é fim de palavra e seus filhos.
+    def __init__(self,char):
+        self.char = char
+        self.isEnd = False        
+        self.players = []
+        self.children = {}
+        
+class triePositions(object):#Objeto da Árvore Trie, em si. 
+    def __init__(self):
+        self.root = trieNodePositions("")
+        
+    def insert(self,line):       
+        chunks = line.split(',')        
+        soFifaId = int(chunks[0])            
+
+        for tag in chunks[2:]:            
+            node = self.root
+            if(tag[0] == "\""):
+                tag = tag[1:]
+            if(tag[0] == " "):
+                tag = tag[1:]
+            if(tag[-1] == "\n"):
+                tag = tag[:-1]
+            if(tag[-1] == "\""):
+                tag = tag[:-1]      
+            for char in tag:                  
+                if  char in node.children:
+                    node = node.children[char]
+                else:
+                    newNode = trieNodePositions(char)
+                    node.children[char] = newNode
+                    node = newNode
+
+            node.isEnd = True 
+            node.players.append(soFifaId)      
+        
+        
+    def depthFirstSearch(self, node, prefixWord):
+        if node.isEnd:            
+            self.output.append([(prefixWord + node.char),node.players])
             
         for child in node.children.values():
             self.depthFirstSearch(child,prefixWord + node.char)
@@ -97,12 +154,12 @@ def partition(RatingsArray, start, end):
             low = low + 1
 
         if low <= high:
-            RatingsArray[low][1], RatingsArray[high][1] = RatingsArray[high][1], RatingsArray[low][1]
+            RatingsArray[low], RatingsArray[high] = RatingsArray[high], RatingsArray[low]
         else:
             break
 
 
-    RatingsArray[start][1], RatingsArray[high][1] = RatingsArray[high][1], RatingsArray[start][1]
+    RatingsArray[start], RatingsArray[high] = RatingsArray[high], RatingsArray[start]
     return high
 
 def quickSort(RatingsArray, start, end):
